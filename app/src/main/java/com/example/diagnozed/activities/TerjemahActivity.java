c0d42247fc33e864964072b32db80e4aa7e2a0b5
Hawari <naufalhawarden@gmail.com>
2022-07-19 15:48:36 +0700

    updating for hand sign detector activity

--- app/src/main/java/com/example/diagnozed/activities/TerjemahActivity.java
+++ app/src/main/java/com/example/diagnozed/activities/TerjemahActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.diagnozed.DetectorActivity;
 import com.example.diagnozed.databinding.ActivityTerjemahBinding;
 
 public class TerjemahActivity extends AppCompatActivity {
@@ -22,7 +23,7 @@ public class TerjemahActivity extends AppCompatActivity {
 
     private void setListeners() {
         binding.isyaratKeIndo.setOnClickListener(v -> {
-            startActivity(new Intent(getApplicationContext(), IsyaratKeIndoActivity.class));
+            startActivity(new Intent(getApplicationContext(), DetectorActivity.class));
         });
 
         binding.indoKeIsyarat.setOnClickListener(v -> {
